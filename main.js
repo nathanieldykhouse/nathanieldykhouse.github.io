baaf30bb18d537da6cdac29320599c60f48108cf
Clover9898 <62666670+Clover9898@users.noreply.github.com>
2025-03-19 08:29:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-const xyValues = [
-    {x:1, y:5},
-    {x:2, y:3},
-    {x:3, y:5}
-];
-const myChart = new Chart("myChart",{
-    type: "scatter",
-    data: {2,5,3},
-    options: {"a, b, c"}
-});
+
