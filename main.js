a4a802ed681e19092fcdc84551945a02d314e96d
Clover9898 <62666670+Clover9898@users.noreply.github.com>
2025-03-19 11:02:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
-const xyValues = [
-    {x:1, y:5},
-    {x:2, y:3},
-    {x:3, y:5}
-];
-const myChart = new Chart("myChart",{
-    type: "scatter",
-    data: {2,5,3},
-    options: {"a, b, c"}
-});
+//graph type handler
+var selectedGraphType = document.getElementById("graphType");
+selectedGraphType.onchange = (event) =>{
+    var inputText = event.target.value;
+    document.getElementById("verificationText").innerText = inputText;
+    if(inputText == "scatter"){
+        newScatterplot();
+    }
+}
+
+//X and Y axis handler
+function addRow(){
+    let container = document.getElementById("inputContainer");
+    let div = document.createElement("div");
+    div.classList.add("inputRow");
+    div.innerHTML = 
+        <input type="number" placeholder="x-value" class="x-input">
+        <input type="number" placeholder = "y-value" class="y-input">
+        <button onclick="removeRow(this)">Remove</button>
+    ;
+    container.appendChild(div);
+}
+
+//graph generator
+function newScatterplot(){
+    
+    const xyValues = [
+        {x:1, y:5},
+        {x:2, y:3},
+        {x:3, y:5}
+    ];
+    
+    const graphOptions = ["beans", "cheese", "orangutang"];
+    const myChart = new Chart(document.getElementById("graphOutputCanvas"),{
+        type: "scatter",
+        data: {
+            datasets: [{
+                pointRadius: 4,
+                pointBackgroundColor: "rgba(0,0,255,1)",
+                data: xyValues
+            }]
+        },
+        options: {}
+    });
+}
