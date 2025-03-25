4d794e8e46a97cc319f5d917571072c04eee9960
Clover9898 <62666670+Clover9898@users.noreply.github.com>
2025-03-25 08:23:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ var selectedGraphType = document.getElementById("graphType");
 var xAxis = [];
 var yAxis = [];
 var xyAxis = [];
+//analysis handler
+var selectedTableType = document.getElementById("tableType");
 
 selectedGraphType.onchange = (event) =>{
     var inputText = event.target.value;
     resetInputs();
     generateGraph();
+}
+
+selectedTableType.onchange = (event) =>{
+    generateTable();
 }
 
 function checkGraphInfoChange(){
@@ -68,6 +74,7 @@ function generateGraph(){
             break;
         case "donut": newDonutChart();
     }
+    generateTable();
 }
 
 function resetInputs(){
@@ -275,29 +282,146 @@ function newDonutChart(){
     });
 }
 
+//helpful stuff
+
+function checkSort(input){
+    var prevValue = -999999999999999;
+    var isSorted = true;
+    for(let i = 0; i < input.length; i++){
+        if(input[i] < prevValue){
+            isSorted = false;
+        } else{
+            prevValue = input[i];
+        }
+    }
+    return isSorted;
+}
+
+function sortList(rawInput){
+    var sortedList = rawInput;
+    var isSorted = checkSort(sortedList);
+    while(!isSorted){
+        for(let i = 0; i < sortedList.length; i++){
+            let nextValue = sortedList[i+1];
+            let curValue = sortedList[i];
+            if(curValue > nextValue){
+                sortedList[i] = nextValue;
+                sortedList[i+1] = curValue;
+            }
+        }
+        let sorted = checkSort(sortedList);
+        if(sorted == true){
+            isSorted = true;
+        } else{
+            continue;
+        }
+    }
+    return sortedList;
+}
+
 //analysis table
 
+function deletePreviousTable(){
+    let container = document.getElementById("analysisTable");
+    let tableType = document.getElementById("tableTypeDropdown");
+    let containerHead = document.getElementById("analysisTableHead");
+    let containerBody = document.getElementById("analysisTableBody");
+    var childCountHead = containerHead.children.length;
+    var childCountBody = containerBody.children.length;
+    for(let i = 0; i < childCountHead; i++){
+        containerHead.removeChild(containerHead.lastChild);
+    }
+    for(let i = 0; i < childCountBody; i++){
+        containerBody.removeChild(containerBody.lastChild);
+    }
+}
+
+function displayTable(rawValues, percentageList){
+    let container = document.getElementById("analysisTableBody");
+    for(let i = 0; i < xAxis.length; i++){
+        let row = document.createElement("tr");
+        let cell1 = document.createElement("td");
+        let cell2 = document.createElement("td");
+        let cell3 = document.createElement("td");
+        cell1.textContent = xAxis[i];
+        cell2.textContent = rawValues[i];
+        cell3.textContent = (percentageList[i]*100).toFixed(2) + "%";
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        row.appendChild(cell3);
+        container.appendChild(row);
+    }
+}
 
 function getPercentages(rawValues){
     let percentageList = [];
     var sumAllNums = 0;
-    for(var curIndex = 0; curIndex < rawValues.length; curIndex++){
+    for(let i = 0; i < rawValues.length; i++){
         sumAllNums += rawValues[i];
     }
-    for(var i = 0; i < rawValues.length; i++){
+    for(let i = 0; i < rawValues.length; i++){
         let percentage = rawValues[i]/sumAllNums;
-        
+        percentageList.push(percentage);
     }
+    return percentageList;
 }
 
-function buildTable(){
-    let rawValuesQuery = document.querySelectorAll(".y-input");
-    let rawValues = [];
-    for(var i = 0; i < rawValuesQuery.length; i++){
-        let y = parseFloat(rawValuesQuery[i].value);
-        rawValues.push(y);
+function getMean(rawVals){
+    let totalRaw = 0;
+    for(let i = 0; i < rawVals.length; i++){
+        totalRaw += rawVals[i];
     }
-    getPercentages(rawValues);
+    return totalRaw/(rawVals.length-1);
+}
+
+function getMedian(rawVals){
+    let sortedVals = sortList(rawVals);
+}
+
+function buildFrequencyTable(){
+    let rawValues = yAxis;
+    var percentageList = getPercentages(rawValues);
+    displayTable(rawValues, percentageList)
+}
+
+function buildMMMSDTable(){
+    let rawValues = yAxis;
+    let mean = getMean(rawValues);
+    let median = getMedian(rawValues);
+    let mode = 0;
+}
+
+function generateTable(){
+    deletePreviousTable();
+    let tableType = document.getElementById("tableType").value;
+    let thead = document.getElementById("analysisTableHead");
+    if(tableType == "frequency"){
+        let hc1 = document.createElement("th");
+        let hc2 = document.createElement("th");
+        let hc3 = document.createElement("th");
+        hc1.textContent = "Value";
+        hc2.textContent = "Frequency";
+        hc3.textContent = "Relative Frequency";
+        thead.appendChild(hc1);
+        thead.appendChild(hc2);
+        thead.appendChild(hc3);
+        buildFrequencyTable();
+    } else if(tableType == "mmmsd"){
+        let hc1 = document.createElement("th");
+        let hc2 = document.createElement("th");
+        let hc3 = document.createElement("th");
+        let hc4 = document.createElement("th");
+        hc1.textContent = "Mean";
+        hc2.textContent = "Median";
+        hc3.textContent = "Mode";
+        hc4.textContent = "Standard Deviation";
+        thead.appendChild(hc1);
+        thead.appendChild(hc2);
+        thead.appendChild(hc3);
+        thead.appendChild(hc4);
+        buildMMMSDTable();
+    }
+    
 }
 
 resetInputs();
