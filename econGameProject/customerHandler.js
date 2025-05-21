265806a2f6f5bcd7cf46c3fac72459ee07039e57
Clover9898 <62666670+Clover9898@users.noreply.github.com>
2025-05-21 10:05:40 -0400

    Update customerHandler.js]

--- econGameProject/customerHandler.js
+++ econGameProject/customerHandler.js
@@ -1,7 +1,7 @@
 var allCustomers = [];
 
-var firstNames = ["Jane", "John", "Jack"];
-var lastNames = ["Smith", "Taylor", "House"];
+var firstNames = ["Jane", "John", "Jack", "Kaitlyn"];
+var lastNames = ["Smith", "Taylor", "House", "Herrema", "Bisbee"];
 
 function generateLoanRequestAmt(financialStatus){
     switch(financialStatus){
@@ -15,8 +15,8 @@ function generateLoanRequestAmt(financialStatus){
 }
 
 function generateNewCustomer(){
-    let firstName = firstNames[generateRandom(0, firstNames.length)];
-    let lastName = lastNames[generateRandom(0, lastNames.length)];
+    let firstName = firstNames[generateRandom(0, firstNames.length-1)];
+    let lastName = lastNames[generateRandom(0, lastNames.length-1)];
     let fullName = firstName + " " + lastName;
     //0 - poor, 1 - middle income, 2 - rich
     let financialStatus = generateRandom(0,2);
@@ -24,9 +24,10 @@ function generateNewCustomer(){
     let requestedInterest = defaultInterest;
     let trustworthyness = generateRandom(1,100);
     const customer = {
-        Name: fullname,
+        Name: fullName,
         requestedAmount: loanRequestAmt,
         trustworthyPercent: trustworthyness
     };
     allCustomers.push(customer);
+    updateCustomerInfo(allCustomers.length-1);
 }
