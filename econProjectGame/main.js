da767159a9e53b78093589c9ed95775a96cd3162
Clover9898 <62666670+Clover9898@users.noreply.github.com>
2025-05-28 12:05:53 -0400

    Update main.js

--- econProjectGame/main.js
+++ econProjectGame/main.js
@@ -1,3 +1,3 @@
 window.addEventListener('click', function(){
-  window.location.href = "file:///home/chronos/u-ebf9c2623faf7affcfb6f1cadf474b20b1e795f5/MyFiles/Downloads/AchingGivingSequences/econProjectGame/mainGameFiles/index.html";
+  window.location.href = "https://clover9898.github.io/econProjectGame/mainGameFiles/index.html";
 })
