0accb84523abe6385a5afeb4a518acf76094b788
Clover9898 <62666670+Clover9898@users.noreply.github.com>
2025-05-28 12:01:47 -0400

    Add files via upload

---
+++ econProjectGame/main.js
@@ -0,0 +1,3 @@
+window.addEventListener('click', function(){
+  window.location.href = "file:///home/chronos/u-ebf9c2623faf7affcfb6f1cadf474b20b1e795f5/MyFiles/Downloads/AchingGivingSequences/econProjectGame/mainGameFiles/index.html";
+})
