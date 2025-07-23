9c53e3a6cc3c38739d87f870f24b3b8612e0ff0a
Clover9898 <62666670+Clover9898@users.noreply.github.com>
2025-07-22 22:05:11 -0400

    Update main.js

--- projectFiles/econProjectGame/main.js
+++ projectFiles/econProjectGame/main.js
@@ -1,3 +1,3 @@
 window.addEventListener('click', function(){
-  window.location.href = "https://clover9898.github.io/econProjectGame/mainGameFiles/index.html";
+  window.location.href = "https://clover9898.github.io/projectFiles/econProjectGame/mainGameFiles/index.html";
 })
