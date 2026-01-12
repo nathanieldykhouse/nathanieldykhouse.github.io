2829892bbd532180a652fb6d1555e4c0b4430085
Nathaniel <78817041+nathanieldykhouse@users.noreply.github.com>
2026-01-12 09:08:25 -0500

    Update freethrow.js

--- otherhosting/freethrow.js
+++ otherhosting/freethrow.js
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(){
-    MMAPPLETS.UI.setDisplayedSection(1);
-    MMAPPLETS.INFO.getAppletInfo();
-    MMAPPLETS.UI.addClickListeners();
+    
 });
